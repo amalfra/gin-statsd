dc471c49656ec982c95d1f051e7db9afff84bbf3
Amal <amalfra@gmail.com>
2018-07-06 00:42:00 +0530

    add more tests for options

--- middleware/options_test.go
+++ middleware/options_test.go
@@ -21,3 +21,22 @@ func TestDefaultRequestKey(t *testing.T) {
 		t.Error("Incorrect requestKey generated")
 	}
 }
+
+func TestNonDefaultAddress(t *testing.T) {
+	host := "abcd"
+	port := 123
+	oi := &Options{Host: host, Port: port}
+	addr := oi.getAddress()
+	if !strings.Contains(addr, host) || !strings.Contains(addr, strconv.Itoa(port)) {
+		t.Error("Incorrect address generated")
+	}
+}
+
+func TestNonDefaultRequestKey(t *testing.T) {
+	key := "abcd"
+	oi := &Options{RequestKey: key}
+	reqKey := oi.getRequestKey()
+	if reqKey != key {
+		t.Error("Incorrect requestKey generated")
+	}
+}
